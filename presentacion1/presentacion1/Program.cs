08f07c7249f682b4f94aea52ec9b9f60deef6101
deimerhdz <deimerhdz21@gmail.com>
2023-06-17 11:59:44 -0500

    Agregando presentacion y tipos de datos

---
+++ presentacion1/presentacion1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace presentacion1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Este es un comentario
+            /*
+              Este es un comentario de bloque
+             */
+
+            Console.WriteLine("Hola mundo.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ tipos-de-datos/tipos-de-datos/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tipos_de_datos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad = 0;
+            decimal precio = 0.0m;
+            float estatura = 0.0f;
+            double promedio = 0.00;
+            string nombre = "Deimer";
+            bool mayorEdad = false;
+            DateTime fecha = DateTime.MinValue;
+
+            Console.WriteLine("EL valor de edad es: {0} ",edad);
+            Console.WriteLine("EL valor de precio es: {0:C} ",precio);
+            Console.WriteLine("EL valor de estatura es: {0:F2} ", estatura);
+            Console.WriteLine("EL valor de promedio es: {0:F2} ", promedio);
+            Console.WriteLine("EL valor de nombre es: "+nombre);
+            Console.WriteLine("EL valor de mayorEdad es: "+ mayorEdad);
+            Console.WriteLine("EL valor de fecha es: "+ fecha.ToShortDateString());
+            Console.ReadKey();
+
+
+            
+
+
+        }
+    }
+}
